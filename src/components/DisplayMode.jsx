723c995475cdeac50b06816872c7b82879087cc7
Wesley Wirmond <wesleywirmond@gmail.com>
2022-10-30 20:05:06 -0300

    save theme option in sessionStorage

--- src/components/DisplayMode.jsx
+++ src/components/DisplayMode.jsx
@@ -10,6 +10,7 @@ export default class DisplayMode extends Component {
         this.lightThemeButton = React.createRef();
         this.activeLightTheme = this.activeLightTheme.bind(this);
         this.activeDarkTheme = this.activeDarkTheme.bind(this);
+        this.loadTheme();
     }
 
     activeLightTheme = (event) => {
@@ -17,18 +18,37 @@ export default class DisplayMode extends Component {
         this.lightThemeButton.current.classList.add('active');
         document.body.classList.remove('dark');
         document.body.classList.add('light');
+        this.saveOptionInSessionStorage('light');
     }
+
     activeDarkTheme = (event) => {
         this.lightThemeButton.current.classList.remove('active');
         this.darkThemeButton.current.classList.add('active');
         document.body.classList.remove('light');
         document.body.classList.add('dark');
+        this.saveOptionInSessionStorage('dark');
+    }
+
+    loadTheme = () => {
+        return document.body.classList.add(this.getThemeOption());
+    }
+
+    saveOptionInSessionStorage = (theme = 'light') => {
+        return sessionStorage.setItem("theme-option", theme);
+    }
+
+    getThemeOption = () => {
+        if(!(sessionStorage.getItem("theme-option"))){
+            return 'light';
+        }
+
+        return sessionStorage.getItem("theme-option");
     }
 
     render() {
         return (
             <div className="display-mode">
-                <button className="light-theme-button active" ref={this.lightThemeButton} onClick={this.activeLightTheme}><span className="icon"><FontAwesomeIcon icon={faSun} /></span></button>
+                <button className="light-theme-button" ref={this.lightThemeButton} onClick={this.activeLightTheme}><span className="icon"><FontAwesomeIcon icon={faSun} /></span></button>
                 <button className="dark-theme-button" ref={this.darkThemeButton} onClick={this.activeDarkTheme}><span className="icon"><FontAwesomeIcon icon={faMoon} /></span></button>
             </div>
         )
