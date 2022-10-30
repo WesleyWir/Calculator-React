c446aa761e4134f4a8111c3b26d74f6a38c01b6d
Wesley Wirmond <wesleywirmond@gmail.com>
2022-10-30 19:53:49 -0300

    implemented dark and light theme partially

--- src/components/DisplayMode.jsx
+++ src/components/DisplayMode.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 import './../assets/css/components/DisplayMode.css';
 
-export default props =>
-    <div className="display-mode">
-        <button class="light-theme-button active"><span class="icon"><FontAwesomeIcon icon={faSun} /></span></button>
-        <button class="dark-theme-button"><span class="icon"><FontAwesomeIcon icon={faMoon} /></span></button>
-    </div>
+export default class DisplayMode extends Component {
+    constructor(props) {
+        super(props);
+        this.darkThemeButton = React.createRef();
+        this.lightThemeButton = React.createRef();
+        this.activeLightTheme = this.activeLightTheme.bind(this);
+        this.activeDarkTheme = this.activeDarkTheme.bind(this);
+    }
+
+    activeLightTheme = (event) => {
+        this.darkThemeButton.current.classList.remove('active');
+        this.lightThemeButton.current.classList.add('active');
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    }
+    activeDarkTheme = (event) => {
+        this.lightThemeButton.current.classList.remove('active');
+        this.darkThemeButton.current.classList.add('active');
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }
+
+    render() {
+        return (
+            <div className="display-mode">
+                <button className="light-theme-button active" ref={this.lightThemeButton} onClick={this.activeLightTheme}><span className="icon"><FontAwesomeIcon icon={faSun} /></span></button>
+                <button className="dark-theme-button" ref={this.darkThemeButton} onClick={this.activeDarkTheme}><span className="icon"><FontAwesomeIcon icon={faMoon} /></span></button>
+            </div>
+        )
+    }
+}    
